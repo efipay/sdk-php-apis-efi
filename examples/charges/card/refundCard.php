5026139bda40367dcd14676e342ec42da358c848
Guilherme Cotta <guilherme.cota@sejaefi.com.br>
2024-07-19 14:58:38 -0300

    ✨feat: Added a new example

---
+++ examples/charges/card/refundCard.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Detailed endpoint documentation
+ * https://dev.efipay.com.br/docs/api-cobrancas/cartao#estorno-de-pagamento-via-cartão-de-crédito
+ */
+
+$autoload = realpath(__DIR__ . "/../../../vendor/autoload.php");
+if (!file_exists($autoload)) {
+	die("Autoload file not found or on path <code>$autoload</code>.");
+}
+require_once $autoload;
+
+use Efi\Exception\EfiException;
+use Efi\EfiPay;
+
+$optionsFile = __DIR__ . "/../../credentials/options.php";
+if (!file_exists($optionsFile)) {
+	die("Options file not found or on path <code>$options</code>.");
+}
+$options = include $optionsFile;
+
+$params = [
+	"id" => 0
+];
+
+$body = [
+	"amount" => 1000
+];
+
+try {
+	$api = new EfiPay($options);
+	$response = $api->refundCard($params, $body);
+
+	if (isset($options["responseHeaders"]) && $options["responseHeaders"]) {
+		print_r("<pre>" . json_encode($response->body, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) . "</pre>");
+		print_r("<pre>" . json_encode($response->headers, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) . "</pre>");
+	} else {
+		print_r("<pre>" . json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) . "</pre>");
+	}
+} catch (EfiException $e) {
+	print_r($e->code . "<br>");
+	print_r($e->error . "<br>");
+	print_r($e->errorDescription) . "<br>";
+	if (isset($options["responseHeaders"]) && $options["responseHeaders"]) {
+		print_r("<pre>" . json_encode($e->headers, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) . "</pre>");
+	}
+} catch (Exception $e) {
+	print_r($e->getMessage());
+}
