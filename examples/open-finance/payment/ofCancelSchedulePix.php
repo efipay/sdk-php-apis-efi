a678d02958ba0d91d7bb7cd4d87d8ef0cf97cf9c
Guilherme Cotta <guilherme.cota@sejaefi.com.br>
2024-07-19 14:57:18 -0300

    ✨feat: Added a new example

---
+++ examples/open-finance/payment/ofCancelSchedulePix.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Detailed endpoint documentation
+ * hhttps://dev.efipay.com.br/docs/api-open-finance/devolucao#cancelar-um-pagamento-agendado
+ */
+
+$autoload = realpath(__DIR__ . "/../../../vendor/autoload.php");
+if (!file_exists($autoload)) {
+	die("Autoload file not found or on path <code>$autoload</code>.");
+}
+require_once $autoload;
+
+use Efi\Exception\EfiException;
+use Efi\EfiPay;
+
+$optionsFile = __DIR__ . "/../../credentials/options.php";
+if (!file_exists($optionsFile)) {
+	die("Options file not found or on path <code>$options</code>.");
+}
+$options = include $optionsFile;
+
+$params = [
+	"identificadorPagamento" => "urn:participant:00000000-0000-0000-0000-000000000000",
+];
+
+try {
+	$api = new EfiPay($options);
+	$response = $api->ofCancelSchedulePix($params);
+
+	if (isset($options["responseHeaders"]) && $options["responseHeaders"]) {
+		print_r("<pre>" . json_encode($response->body, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) . "</pre>");
+		print_r("<pre>" . json_encode($response->headers, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) . "</pre>");
+	} else {
+		print_r("<pre>" . json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) . "</pre>");
+	}
+} catch (EfiException $e) {
+	print_r($e->code . "<br>");
+	print_r($e->error . "<br>");
+	print_r($e->errorDescription) . "<br>";
+	if (isset($options["responseHeaders"]) && $options["responseHeaders"]) {
+		print_r("<pre>" . json_encode($e->headers, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) . "</pre>");
+	}
+} catch (Exception $e) {
+	print_r($e->getMessage());
+}
